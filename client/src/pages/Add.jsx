fd8ec540b137d1399355034ffaa474ec1db16023
Ankeet Mourya <mouryaankeet@gmail.com>
2024-05-11 15:36:42 +0530

    footer changes

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 //git test
+//footer added
 const Add = () => {
   const [book, setBook] = useState({
     title: "",
